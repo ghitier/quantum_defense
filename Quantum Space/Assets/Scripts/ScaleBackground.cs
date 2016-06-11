138730bc0f34b029650a0bce5ffc2954088cb964
Guilaume HITIER <guillaume.hitier@epitech.eu>
2016-06-11 03:20:22 +0200

    Added Bg scaling

---
+++ Quantum Space/Assets/Scripts/ScaleBackground.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleBackground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+ 		float worldScreenHeight = Camera.main.orthographicSize * 2;
+ 		float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+         
+ 		transform.localScale = new Vector3(
+     	worldScreenWidth / sr.sprite.bounds.size.x,
+     	worldScreenHeight / sr.sprite.bounds.size.y, 1);
+	}
+}
