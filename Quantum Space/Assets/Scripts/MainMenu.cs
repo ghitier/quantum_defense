d9129de688598b4da0f5a754d350ee24dff3e7f9
Guilaume HITIER <guillaume.hitier@epitech.eu>
2016-06-11 07:55:27 +0200

    A plus en LAN

---
+++ Quantum Space/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	/* Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+
+	public void play_game()
+	{
+		SceneManager.LoadScene("main", LoadSceneMode.Single);
+	}
+
+	public void quit_game()
+	{
+		Application.Quit();
+	}
+}
